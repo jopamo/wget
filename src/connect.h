aac3fcbce8c85c9a7884ae0d61854f2abdcf7072
Paul <p@1g4.org>
2025-11-23 09:36:53 -0600

    connect: document transport API and make header self contained
    - Add <stdbool.h> include so bool is always defined for standalone users
    - Wrap declarations in extern "C" for C++ compatibility
    - Expand and clarify comments for connect_to_* and socket_* helpers
    - Fully document transport_implementation callbacks and lifetime rules
    - Keep all existing symbols and enums intact while tightening parameter names

--- src/connect.h
+++ src/connect.h
@@ -1,81 +1,168 @@
-/* Declarations for connect.
-   Copyright (C) 1996-2011, 2015, 2018-2024 Free Software Foundation,
-   Inc.
-
-This file is part of GNU Wget.
-
-GNU Wget is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
- (at your option) any later version.
-
-GNU Wget is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Wget.  If not, see <http://www.gnu.org/licenses/>.
-
-Additional permission under GNU GPL version 3 section 7
-
-If you modify this program, or any covered work, by linking or
-combining it with the OpenSSL project's OpenSSL library (or a
-modified version of that library), containing parts covered by the
-terms of the OpenSSL or SSLeay licenses, the Free Software Foundation
-grants you additional permission to convey the resulting work.
-Corresponding Source for a non-source form of such a combination
-shall include the source code for the parts of OpenSSL used as well
-as that of the covered work.  */
+/* Establishing and handling network connections
+ * src/connect.h
+ */
 
 #ifndef CONNECT_H
 #define CONNECT_H
 
+#include <stdbool.h>
+
 #include "host.h" /* for definition of ip_address */
 
-/* Function declarations */
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-/* Returned by connect_to_host when host name cannot be resolved.  */
+/* Returned by connect_to_host when host name cannot be resolved */
 enum { E_HOST = -100 };
-int connect_to_host(const char*, int);
-int connect_to_ip(const ip_address*, int, const char*);
 
-int bind_local(const ip_address*, int*);
-int accept_connection(int);
+/* Connect to a remote host name on the given TCP port
+ * Returns a connected socket fd on success or E_HOST / -1 on error
+ */
+int connect_to_host(const char *host, int port);
 
+/* Connect to a specific IP address on the given TCP port
+ * PRINT is an optional host label used only in logs
+ * Returns a connected socket fd on success or -1 on error
+ */
+int connect_to_ip(const ip_address *ip, int port, const char *print);
+
+/* Create a listening socket bound to BIND_ADDRESS:*PORT
+ * If *PORT is 0, the kernel chooses a port and the chosen value is
+ * written back to *PORT
+ * Returns a listening socket fd on success or -1 on error
+ */
+int bind_local(const ip_address *bind_address, int *port);
+
+/* Accept a single incoming connection on LOCAL_SOCK
+ * Returns a new connected socket fd on success or -1 on error
+ */
+int accept_connection(int local_sock);
+
+/* Endpoint selector for socket_ip_address and socket_family */
 enum { ENDPOINT_LOCAL, ENDPOINT_PEER };
-bool socket_ip_address(int, ip_address*, int);
+
+/* Query the IP address associated with an existing connection
+ * When ENDPOINT_LOCAL is used, returns the local side of the socket
+ * When ENDPOINT_PEER is used, returns the remote side of the socket
+ * Returns true on success, false on error
+ */
+bool socket_ip_address(int sock, ip_address *ip, int endpoint);
+
+/* Return the socket family (AF_INET, AF_INET6, ...) of a connection
+ * Returns the family on success or -1 on error
+ */
 int socket_family(int sock, int endpoint);
 
-bool retryable_socket_connect_error(int);
+/* Return true if a connect error is considered retryable by wget
+ * Non retryable include protocol support errors and optionally
+ * connection refused / unreachable depending on opt.retry_connrefused
+ */
+bool retryable_socket_connect_error(int err);
 
-/* Flags for select_fd's WAIT_FOR argument. */
+/* Flags for select_fd's WAIT_FOR argument */
 enum { WAIT_FOR_READ = 1, WAIT_FOR_WRITE = 2 };
-int select_fd(int, double, int);
-bool test_socket_open(int);
 
+/* Wait for FD to become ready for reading and/or writing
+ * MAXTIME is a timeout in seconds
+ * Returns 1 if ready, 0 on timeout, -1 on error
+ */
+int select_fd(int fd, double maxtime, int wait_for);
+
+/* Lightweight test that a socket is still open from wget's point of view
+ * Returns true if the connection appears open, false if it has pending
+ * data or EOF/error ready to be read
+ */
+bool test_socket_open(int sock);
+
+/* Transport abstraction for non plain file descriptor backends
+ *
+ * Implementations typically wrap an underlying socket with SSL or
+ * another transport, while preserving fd based APIs for the caller
+ */
 struct transport_implementation {
-  int (*reader)(int, char*, int, void*, double);
-  int (*writer)(int, char*, int, void*);
-  int (*poller)(int, double, int, void*);
-  int (*peeker)(int, char*, int, void*, double);
-  const char* (*errstr)(int, void*);
-  void (*closer)(int, void*);
+  /* Read at most BUF_LEN bytes into BUF
+   * Returns number of bytes read, 0 on EOF, or -1 on error
+   */
+  int (*reader)(int fd, char *buf, int buf_len, void *ctx, double timeout);
+
+  /* Write up to BUF_LEN bytes from BUF
+   * Returns number of bytes written or -1 on error
+   */
+  int (*writer)(int fd, char *buf, int buf_len, void *ctx);
+
+  /* Poll FD for readiness based on WAIT_FOR flags
+   * Returns 1 if ready, 0 on timeout, -1 on error
+   */
+  int (*poller)(int fd, double timeout, int wait_for, void *ctx);
+
+  /* Peek at available data without consuming it
+   * Returns number of bytes that can be read, 0 on EOF, or -1 on error
+   */
+  int (*peeker)(int fd, char *buf, int buf_len, void *ctx, double timeout);
+
+  /* Optional transport specific error description
+   * Returns a borrowed pointer which must remain valid until fd_close
+   */
+  const char *(*errstr)(int fd, void *ctx);
+
+  /* Close the underlying transport and release any associated state */
+  void (*closer)(int fd, void *ctx);
 };
 
-void fd_register_transport(int, struct transport_implementation*, void*);
-void* fd_transport_context(int);
-int fd_read(int, char*, int, double);
-int fd_write(int, char*, int, double);
-int fd_peek(int, char*, int, double);
-const char* fd_errstr(int);
-void fd_close(int);
+/* Register a transport implementation for an existing socket like FD
+ * After registration, fd_read/fd_write/fd_peek/fd_errstr/fd_close will
+ * dispatch to the provided implementation where possible
+ */
+void fd_register_transport(int fd,
+                           struct transport_implementation *imp,
+                           void *ctx);
+
+/* Retrieve the opaque context pointer previously registered with FD
+ * Returns NULL if no transport is registered
+ */
+void *fd_transport_context(int fd);
+
+/* Read from FD with an optional timeout in seconds
+ * If TIMEOUT is -1, opt.read_timeout will be used
+ * Returns number of bytes read, 0 on EOF, or -1 on error
+ */
+int fd_read(int fd, char *buf, int buf_len, double timeout);
+
+/* Write the contents of BUF to FD with an optional timeout in seconds
+ * Loops until all data is written or an error occurs
+ * Returns last write result, or -1 on error
+ */
+int fd_write(int fd, char *buf, int buf_len, double timeout);
+
+/* Peek at pending data on FD without consuming it
+ * Returns number of bytes peeked, 0 on EOF, or -1 on error
+ */
+int fd_peek(int fd, char *buf, int buf_len, double timeout);
+
+/* Return a human readable error string for the most recent fd_* failure
+ * May return a transport specific message or fall back to strerror(errno)
+ */
+const char *fd_errstr(int fd);
+
+/* Close FD and any transport layered on top of it
+ * Safe to call with a negative fd, which is ignored
+ */
+void fd_close(int fd);
+
+/* Free internal connect/transport state
+ * Only used in debug and test configurations
+ */
 void connect_cleanup(void);
 
 #ifdef WINDOWS
-int select_fd_nb(int, double, int);
+int select_fd_nb(int fd, double maxtime, int wait_for);
 #else
 #define select_fd_nb select_fd
+#endif
+
+#ifdef __cplusplus
+} /* extern "C" */
 #endif
 
 #endif /* CONNECT_H */
