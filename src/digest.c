baa27e6165f9d380c0b577352fe76486343d3fbd
Paul <p@1g4.org>
2025-11-23 10:03:43 -0600

    digest: factor common streaming helper and simplify includes
    - Add a generic digest_stream_common() helper and small adapter
      thunks for md5/sha1/sha256 to remove duplicated fread/update/final
      loops while preserving existing behavior and error semantics
    - Make streaming helpers validate stream and result pointers before use
    - Drop unused errno/string includes and include <stdio.h> explicitly
      for FILE and BUFSIZ
    - Keep public digest init/process/finish API and sha256_buffer()
      semantics unchanged for existing callers

--- src/digest.c
+++ src/digest.c
@@ -1,12 +1,13 @@
-/* Digest wrappers built on top of nettle or OpenSSL.  */
+/* Digest wrappers built on top of nettle or OpenSSL
+ * src/digest.c
+ */
 
 #include "wget.h"
 #include "md5.h"
 #include "sha1.h"
 #include "sha256.h"
 
-#include <errno.h>
-#include <string.h>
+#include <stdio.h>
 
 #ifdef HAVE_NETTLE
 #include <nettle/md5.h>
@@ -38,19 +39,6 @@ void md5_finish_ctx(struct md5_ctx* ctx, void* resbuf) {
 #endif
 }
 
-int md5_stream(FILE* stream, void* resblock) {
-  struct md5_ctx ctx;
-  md5_init_ctx(&ctx);
-  unsigned char buffer[BUFSIZ];
-  size_t n;
-  while ((n = fread(buffer, 1, sizeof(buffer), stream)) > 0)
-    md5_process_bytes(buffer, n, &ctx);
-  if (ferror(stream))
-    return -1;
-  md5_finish_ctx(&ctx, resblock);
-  return 0;
-}
-
 void sha1_init_ctx(struct sha1_ctx* ctx) {
 #ifdef HAVE_NETTLE
   nettle_sha1_init(ctx);
@@ -73,19 +61,6 @@ void sha1_finish_ctx(struct sha1_ctx* ctx, void* resbuf) {
 #else
   SHA1_Final(resbuf, &ctx->impl);
 #endif
-}
-
-int sha1_stream(FILE* stream, void* resblock) {
-  struct sha1_ctx ctx;
-  sha1_init_ctx(&ctx);
-  unsigned char buffer[BUFSIZ];
-  size_t n;
-  while ((n = fread(buffer, 1, sizeof(buffer), stream)) > 0)
-    sha1_process_bytes(buffer, n, &ctx);
-  if (ferror(stream))
-    return -1;
-  sha1_finish_ctx(&ctx, resblock);
-  return 0;
 }
 
 void sha256_init_ctx(struct sha256_ctx* ctx) {
@@ -112,21 +87,90 @@ void sha256_finish_ctx(struct sha256_ctx* ctx, void* resbuf) {
 #endif
 }
 
-int sha256_stream(FILE* stream, void* resblock) {
-  struct sha256_ctx ctx;
-  sha256_init_ctx(&ctx);
+/* Generic streaming helper used by all digest_stream functions
+ * INIT   initializes the context pointed to by CTX
+ * UPDATE feeds data into the digest
+ * FINISH writes the final digest into RESBLOCK
+ */
+typedef void (*digest_init_fn)(void* ctx);
+typedef void (*digest_update_fn)(void* ctx, const void* buffer, size_t len);
+typedef void (*digest_finish_fn)(void* ctx, void* resblock);
+
+static int digest_stream_common(FILE* stream, void* ctx, digest_init_fn init, digest_update_fn update, digest_finish_fn finish, void* resblock) {
   unsigned char buffer[BUFSIZ];
   size_t n;
+
+  if (!stream || !resblock)
+    return -1;
+
+  init(ctx);
+
   while ((n = fread(buffer, 1, sizeof(buffer), stream)) > 0)
-    sha256_process_bytes(buffer, n, &ctx);
+    update(ctx, buffer, n);
+
   if (ferror(stream))
     return -1;
-  sha256_finish_ctx(&ctx, resblock);
+
+  finish(ctx, resblock);
   return 0;
+}
+
+/* Thin adapters so we can use the generic helper above */
+
+static void md5_init_any(void* ctx) {
+  md5_init_ctx((struct md5_ctx*)ctx);
+}
+
+static void md5_update_any(void* ctx, const void* buffer, size_t len) {
+  md5_process_bytes(buffer, len, (struct md5_ctx*)ctx);
+}
+
+static void md5_finish_any(void* ctx, void* resblock) {
+  md5_finish_ctx((struct md5_ctx*)ctx, resblock);
+}
+
+static void sha1_init_any(void* ctx) {
+  sha1_init_ctx((struct sha1_ctx*)ctx);
+}
+
+static void sha1_update_any(void* ctx, const void* buffer, size_t len) {
+  sha1_process_bytes(buffer, len, (struct sha1_ctx*)ctx);
+}
+
+static void sha1_finish_any(void* ctx, void* resblock) {
+  sha1_finish_ctx((struct sha1_ctx*)ctx, resblock);
+}
+
+static void sha256_init_any(void* ctx) {
+  sha256_init_ctx((struct sha256_ctx*)ctx);
+}
+
+static void sha256_update_any(void* ctx, const void* buffer, size_t len) {
+  sha256_process_bytes(buffer, len, (struct sha256_ctx*)ctx);
+}
+
+static void sha256_finish_any(void* ctx, void* resblock) {
+  sha256_finish_ctx((struct sha256_ctx*)ctx, resblock);
+}
+
+int md5_stream(FILE* stream, void* resblock) {
+  struct md5_ctx ctx;
+  return digest_stream_common(stream, &ctx, md5_init_any, md5_update_any, md5_finish_any, resblock);
+}
+
+int sha1_stream(FILE* stream, void* resblock) {
+  struct sha1_ctx ctx;
+  return digest_stream_common(stream, &ctx, sha1_init_any, sha1_update_any, sha1_finish_any, resblock);
+}
+
+int sha256_stream(FILE* stream, void* resblock) {
+  struct sha256_ctx ctx;
+  return digest_stream_common(stream, &ctx, sha256_init_any, sha256_update_any, sha256_finish_any, resblock);
 }
 
 void sha256_buffer(const void* buffer, size_t len, void* resblock) {
   struct sha256_ctx ctx;
+
   sha256_init_ctx(&ctx);
   sha256_process_bytes(buffer, len, &ctx);
   sha256_finish_ctx(&ctx, resblock);
