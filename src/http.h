c5963f9cecfbb0f320c70a1777dffdc4b330ce39
Paul <p@1g4.org>
2025-11-24 11:38:22 -0600

    Add async HTTP transaction structures and functions
    * Define http_transaction_state enum for async HTTP state machine
    * Add http_transaction_ctx struct to manage async operations
    * Declare http_loop_start_async, http_loop_continue_async, http_loop_cleanup functions
    * Include http_body.h for callback definitions
    * Remove legacy http_loop function declaration

--- src/http.h
+++ src/http.h
@@ -36,20 +36,92 @@
 
 #include "hsts.h"
 #include "transfer.h"
+#include "http_body.h"  // For http_body_done_cb
 
 struct url;
+struct request;
+struct http_stat;
+struct iri;
+struct ip_address;
 
-uerr_t http_loop(const struct url*, struct url*, char**, char**, const char*, int*, struct url*, struct iri*, struct transfer_context*);
-#ifdef ENABLE_COOKIES
-void save_cookies(void);
-#endif
+typedef enum {
+  HLS_INIT = 0,
+  HLS_ESTABLISH_CONNECTION,
+  HLS_SEND_REQUEST,
+  HLS_READ_RESPONSE_HEADERS,
+  HLS_READ_BODY_UNAUTHORIZED,
+  HLS_READ_BODY_REDIRECTED,
+  HLS_READ_BODY_ERROR,
+  HLS_READ_BODY_MAIN,
+  HLS_COMPLETED,
+  HLS_FAILED
+} http_transaction_state;
+
+struct http_transaction_ctx {
+  http_transaction_state state;
+  const struct url* u;
+  struct url* original_url;
+  char** newloc;
+  char** local_file;
+  const char* referer;
+  int* dt;
+  struct url* proxy;
+  struct iri* iri;
+  struct transfer_context* tctx;
+  int count;  // retry count
+
+  // Variables from original gethttp
+  struct request* req;
+  char* type;
+  char *user, *passwd;
+  char* proxyauth;
+  int statcode;
+  int write_error;
+  wgint contlen, contrange;
+  const struct url* conn;
+  FILE* fp;
+  int err;
+  uerr_t retval;
+  int sock;
+  bool auth_finished;
+  bool basic_auth_finished;
+  bool ntlm_seen;
+  bool using_ssl;
+  bool head_only;
+  bool cond_get;
+  char* head;
+  struct http_response* resp;
+  char hdrval[512];
+  char* message;
+  bool warc_enabled;
+  FILE* warc_tmp;
+  char warc_timestamp_str[21];
+  char warc_request_uuid[48];
+  ip_address warc_ip_buf, *warc_ip;
+  off_t warc_payload_offset;
+  bool keep_alive;
+  bool chunked_transfer_encoding;
+  bool inhibit_keep_alive;
+  wgint body_data_size;
+  struct http_stat hstat;  // HTTP status
+
+  // Callback to signal completion of the async operation
+  void (*final_cb)(struct http_transaction_ctx* ctx, uerr_t status);
+};
+
+// Asynchronous HTTP transaction function
+struct http_transaction_ctx* http_loop_start_async(const struct url* u,
+                                                   struct url* original_url,
+                                                   char** newloc,
+                                                   char** local_file,
+                                                   const char* referer,
+                                                   int* dt,
+                                                   struct url* proxy,
+                                                   struct iri* iri,
+                                                   struct transfer_context* tctx,
+                                                   void (*final_cb)(struct http_transaction_ctx* ctx, uerr_t status));
+void http_loop_continue_async(struct http_transaction_ctx* ctx, uerr_t prev_op_status);
+void http_loop_cleanup(struct http_transaction_ctx* ctx);
 void http_cleanup(void);
-time_t http_atotm(const char*);
-
-typedef struct {
-  /* A token consists of characters in the [b, e) range. */
-  const char *b, *e;
-} param_token;
-bool extract_param(const char**, param_token*, param_token*, char, bool*);
 
 #endif /* HTTP_H */
